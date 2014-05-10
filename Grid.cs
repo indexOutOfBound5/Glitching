42628a829fd9b210fc2a53d29d3d10fdc28d8419
DarrenSweeney <sweeney1995@live.co.uk>
2014-05-10 11:25:42 +0100

    Darren Sweeney

---
+++ Grid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Dazzer
+{
+    class Grid
+    {
+        int[,] gridMap;
+        Texture2D texture;
+        int width = 100;
+        int height = 100;
+
+        public Grid(ContentManager content)
+        {
+            gridMap = new int[width, height];
+            texture = content.Load<Texture2D>("Rock");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for (int x = 3; x < width - 3; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    spriteBatch.Draw(texture, new Rectangle(x * 35, y * 35, 30, 30), Color.White);
+                }
+            }
+            spriteBatch.End();
+        }
+    }
+}
---
+++ StateManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dazzer
+{
+    class StateManager
+    {
+        public enum GameState
+        {
+            main_Menu,
+            game_Level
+        }
+
+        public GameState currentGameState = GameState.game_Level;
+    }
+}
